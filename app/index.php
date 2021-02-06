62e6026cba451cac2f70835ee4727abc1ac91c17
tamanoir <martin.saldinger@gmail.com>
2021-02-06 23:43:05 +0000

    update

--- app/index.php
+++ app/index.php
@@ -7,4 +7,18 @@ print_r($_GET);
 echo "</pre>";
 
 
-?>
+
+
+
+switch ($_GET['controller']) {
+    case "login":
+        print_r($_GET);
+        
+
+        break;
+    case "register":
+        print_r($_GET);
+
+
+        break;
+}
